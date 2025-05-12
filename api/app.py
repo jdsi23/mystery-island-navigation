5aa9ff9ef1e07f3813df9cf9144548231e1d77aa
jdsi23 <jameslusk535@gmail.com>
2025-05-12 18:57:37 +0000

    Start Changed

--- api/app.py
+++ api/app.py
@@ -3,14 +3,14 @@ from flask import Flask, send_from_directory, jsonify
 import os
 import json
 
-app = Flask(__name__, static_folder='webapp')
+app = Flask(__name__, static_folder='webapp', static_url_path='')
 
 @app.route('/')
 def serve_index():
     return send_from_directory('webapp', 'index.html')
 
 @app.route('/<path:path>')
-def serve_static(path):
+def serve_static_file(path):
     return send_from_directory('webapp', path)
 
 @app.route('/data/attractions.json')
